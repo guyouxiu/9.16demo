7550ec3f498d46685968fe8c59b9b9a359778b1c
guyouxiu <1063860931@qq.com>
2022-09-16 16:25:46 +0800

    登陆

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,18 +15,35 @@
         show-password
       >
       </el-input>
-      <el-button type="primary">Login</el-button>
+      <el-button type="primary" @click="login">Login</el-button>
+      <el-alert
+          title="密码或者账号错误"
+          type="error"
+          center
+          show-icon
+          v-show="flag"
+        ></el-alert>;
     </div>
   </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            input1:'',
-            input2:'',
-        }
+  data() {
+    return {
+      input1: "",
+      input2: "",
+      flag:false
+    };
+  },
+  methods: {
+    login() {
+      if (this.input1 == "admin" || this.input2 == "123456") {
+        this.$router.push("/");
+      } else {
+        this.flag=true
+      } 
     },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -41,15 +58,21 @@ export default {
     padding-top: 100px;
   }
 }
-.el-input{
-    margin-top: 40px;
+.el-input {
+  margin-top: 40px;
 }
 h1 {
   text-align: center;
   color: #fff;
 }
-:deep .el-input__inner{
-    background-color: #2d3a4b;
-    border: 1px solid #3a4554;
+:deep .el-input__inner {
+  background-color: #2d3a4b;
+  border: 1px solid #3a4554;
+  color: #fff;
+}
+.el-button {
+  width: 500px;
+  height: 40px;
+  margin-top: 20px;
 }
 </style>
